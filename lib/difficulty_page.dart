8fa83d2b4d97f059eb15318c139df8ac5d52c00d
Benediktus Albert <albertgosyanto@gmail.com>
2023-04-29 17:11:07 -0700

    added navigator.push from home page to game page

--- lib/difficulty_page.dart
+++ lib/difficulty_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'game_page.dart';
+import 'game_state_view_model.dart';
 //need to start using viewModels. we can talk about the best setup,
 //then I can do the code from there.
 
@@ -13,68 +15,74 @@ class DifficultyPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //viewmodels declared here
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        /*
-        SizedBox(
-          width: 300.0,
-          height: 50.0,
-          child: ElevatedButton.icon(
+    final gameStateViewModel = context.watch<GameStateViewModel>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Level ${gameStateViewModel.getGameState.getCurrentLevel}"),
+      ),
+      body: Column (
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          /*
+          SizedBox(
+            width: 300.0,
+            height: 50.0,
+            child: ElevatedButton.icon(
+                onPressed: () {
+                  //call viewModel, set to easy
+                  //call game_page (after difficulty set)
+                }, //navigation happens here!
+                icon: Icon(
+                  Icons.directions_run,
+                  size: 24.0,
+                ),
+              label: Text("Easy"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                foregroundColor: Colors.white,
+              ),
+            ),
+          ),
+          const SizedBox(height:30),
+          SizedBox(
+            width: 300.0,
+            height: 50.0,
+            child: ElevatedButton.icon(
               onPressed: () {
-                //call viewModel, set to easy
-                //call game_page (after difficulty set)
-              }, //navigation happens here!
+              },
               icon: Icon(
-                Icons.directions_run,
+                Icons.directions_bike,
                 size: 24.0,
               ),
-            label: Text("Easy"),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.green,
-              foregroundColor: Colors.white,
+              label: Text("Medium"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orangeAccent,
+                foregroundColor: Colors.white,
+              ),
             ),
           ),
-        ),
-        const SizedBox(height:30),
-        SizedBox(
-          width: 300.0,
-          height: 50.0,
-          child: ElevatedButton.icon(
-            onPressed: () {
-            },
-            icon: Icon(
-              Icons.directions_bike,
-              size: 24.0,
-            ),
-            label: Text("Medium"),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.orangeAccent,
-              foregroundColor: Colors.white,
-            ),
-          ),
-        ),
-        const SizedBox(height:30),
-        SizedBox(
-          width: 300.0,
-          height: 50.0,
-          child: ElevatedButton.icon(
-            onPressed: () {}, //navigation happens here!
-            icon: Icon(
-              Icons.directions_car,
-              size: 24.0,
-            ),
-            label: Text("Hard"),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.red,
-              foregroundColor: Colors.white,
+          const SizedBox(height:30),
+          SizedBox(
+            width: 300.0,
+            height: 50.0,
+            child: ElevatedButton.icon(
+              onPressed: () {}, //navigation happens here!
+              icon: Icon(
+                Icons.directions_car,
+                size: 24.0,
+              ),
+              label: Text("Hard"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+              ),
             ),
           ),
-        ),
-         */
-        Container(height: 400, width: 400, child: GamePage()),
-      ],
+           */
+          Container(height: 400, width: 400, child: GamePage()),
+        ],
+      ),
     );
   }
 }
--- lib/firebase/login_register_page.dart
+++ lib/firebase/login_register_page.dart
@@ -1,37 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'login_view.dart';
 import 'register_view.dart';
 import 'package:chimp_game/home_page.dart';
+import 'package:chimp_game/game_state_view_model.dart';
 
 class LoginOrRegister extends StatelessWidget {
   const LoginOrRegister({Key? key, required this.autoLogin}) : super(key: key);
   final bool autoLogin;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chimp Game',
-      routes: {
-        '/login/': (context) => const LoginView(),
-        '/register/': (context) => const RegisterView(),
-      },
-      home: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              if (autoLogin) {
-                return const MyHomePage(pageIndex: 0);
-              } else {
-                return const LoginView();
-              }
-            default:
-              return const Text('Loading...');
-          }
+    return ChangeNotifierProvider(
+      create: (context) => GameStateViewModel(),
+      child: MaterialApp(
+        title: 'Chimp Game',
+        routes: {
+          '/login/': (context) => const LoginView(),
+          '/register/': (context) => const RegisterView(),
         },
+        home: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                if (autoLogin) {
+                  return const MyHomePage(pageIndex: 0);
+                } else {
+                  return const LoginView();
+                }
+              default:
+                return const Text('Loading...');
+            }
+          },
+        ),
       ),
     );
   }
