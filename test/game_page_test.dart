ccf668cfb96985f908e35e1fe22d9f3d6e9de2b5
Benediktus Albert <albertgosyanto@gmail.com>
2023-05-21 20:34:59 -0700

    fixed some issues with game_page_test, added tests for game_page and home_page

--- test/game_page_test.dart
+++ test/game_page_test.dart
@@ -5,46 +5,128 @@ import 'package:go_router/go_router.dart';
 import 'package:chimp_game/game_state_view_model.dart';
 import 'package:chimp_game/game_page.dart';
 
-//TODO: Issues with these tests, we might need to use Mockito instead?
+//TODO: Some issues with the tests: tester.pumpWidgets(GamePage) generates 6 buttons maximum
 
 void main() {
   testWidgets('Checks sequence numbers are displayed', (tester) async {
     //await tester.pumpWidget(const MaterialApp(home: GamePage()));
     final testViewModel = GameStateViewModel();
+    testViewModel.setDifficulty('easy');
+    testViewModel.initializeGame();
+
     await tester.pumpWidget(
-      ChangeNotifierProvider<GameStateViewModel>.value(
+      ChangeNotifierProvider.value(
         value: testViewModel,
-        child: const MaterialApp(
-          home: Scaffold(
-            //by default, game page is set to easy difficulty!
-            body: GamePage()
-          )
-        )
+        child: const MaterialApp(home: GamePage())
       )
     );
+    await tester.pumpAndSettle();
 
     //should see numbers 1-3 as the base grid, and 9 buttons
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('CHIMP GAME: Level 1'), findsOneWidget);
+    expect(find.byIcon(Icons.home), findsOneWidget);
+    expect(find.text('Score: 0'), findsOneWidget);
+    expect(find.text('Lives: '), findsOneWidget);
+    expect(find.byIcon(Icons.favorite), findsNWidgets(3));
+    expect(find.byType(ElevatedButton), findsAtLeastNWidgets(6));
+    //expect(find.text('1'), findsOneWidget);
   });
 
-  testWidgets('Check response when button correct button is pressed', (tester) async {
+  testWidgets('Check response when correct button is pressed', (tester) async {
     //await tester.pumpWidget(const MaterialApp(home: GamePage()));
     final testViewModel = GameStateViewModel();
+    testViewModel.setDifficulty('easy');
+    testViewModel.initializeGame();
+
     await tester.pumpWidget(
-        ChangeNotifierProvider.value(
-            value: testViewModel,
-            child: const MaterialApp(
-                home: Scaffold(
-                  //by default, game page is set to easy difficulty!
-                    body: GamePage()
-                )
-            )
-        )
+      ChangeNotifierProvider.value(
+        value: testViewModel,
+        child: const MaterialApp(home: GamePage())
+      )
     );
+    await tester.pumpAndSettle();
+
+    // Verify that player score starts from 0
+    expect(find.text('Score: 0'), findsOneWidget);
 
     //tap #1, see that score goes up?
-    await tester.tap(find.text('1'));
+    //await tester.tap(find.text('1'));
+    int gridKey = testViewModel.sequence!.keys.firstWhere(
+            (k) => testViewModel.sequence![k] == 1, orElse: () => null);
+    testViewModel.onButtonPressed(gridKey);
     await tester.pump();
     //expect(find.text(''), findsOneWidget);
+    expect(find.text('Score: 10'), findsOneWidget);
+  });
+
+  testWidgets('Check response when wrong sequence is pressed', (tester) async {
+    final testViewModel = GameStateViewModel();
+    testViewModel.setDifficulty('medium');
+    testViewModel.initializeGame();
+
+    await tester.pumpWidget(
+      ChangeNotifierProvider.value(
+        value: testViewModel,
+        child: const MaterialApp(home: GamePage())
+      )
+    );
+    await tester.pumpAndSettle();
+
+    // Verify that player lives starts from 3
+    expect(find.byIcon(Icons.favorite), findsNWidgets(3));
+
+    //Press the wrong sequence intentionally (for now, internally)
+    //await tester.tap(find.text('2'));
+    int falseGridKey = testViewModel.sequence!.keys.firstWhere(
+            (k) => testViewModel.sequence![k] == 2, orElse: () => null);
+    testViewModel.onButtonPressed(falseGridKey);
+    await tester.pump();
+
+    // Verify that the player lose a life when doing so
+    expect(find.byIcon(Icons.favorite), findsNWidgets(2));
+    expect(find.byIcon(Icons.heart_broken), findsOneWidget);
+  });
+
+  testWidgets('Checks navigation via Home icon', (tester) async {
+    final testViewModel = GameStateViewModel();
+    testViewModel.setDifficulty('hard');
+    testViewModel.initializeGame();
+
+    const destinationIdentifier = 'asjkdae7148193sads'; // unique string id
+    final router = GoRouter(
+        routes: [
+          GoRoute(
+              path: '/',
+              builder: (context, _) => const GamePage()
+          ),
+          GoRoute(
+            path: "/home_page/:index",
+            name: "home_page",
+            builder: (context, state) {
+              final index = int.parse(state.pathParameters['index']!);
+              return const Scaffold(body: Text(destinationIdentifier));
+            },
+          ),
+        ]
+    );
+
+    await tester.pumpWidget(
+      ChangeNotifierProvider.value(
+        value: testViewModel,
+        child: MaterialApp.router(routerConfig: router)
+      )
+    );
+    await tester.pumpAndSettle();
+
+    // Verify initial route in GamePage and Home icon is available
+    expect(find.text('CHIMP GAME: Level 1'), findsOneWidget);
+    expect(find.byIcon(Icons.home), findsOneWidget);
+
+    // Press the Home icon to navigate back to home_page
+    await tester.tap(find.byIcon(Icons.home));
+    await tester.pumpAndSettle();
+
+    // Verify that the Home icon navigates properly to home_page
+    expect(find.text(destinationIdentifier), findsOneWidget);
   });
 }
---
+++ test/home_page_test.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
+import 'package:chimp_game/firebase/profile_view.dart';
+import 'package:chimp_game/home_page.dart';
+
+typedef Callback = void Function(MethodCall call);
+
+void setupFirebaseAuthMocks([Callback? customHandlers]) {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setupFirebaseCoreMocks();
+}
+
+void main() async {
+  setupFirebaseAuthMocks();
+  setUpAll(() async {
+    await Firebase.initializeApp();
+  });
+
+  testWidgets('Check every displayed text/button', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: MyHomePage(pageIndex: 0)));
+
+    expect(find.text('CHIMP GAME'), findsOneWidget);
+    expect(find.text('PLAY'), findsOneWidget);
+    expect(find.byIcon(Icons.home), findsOneWidget);
+    expect(find.text('Home'), findsOneWidget);
+    expect(find.byIcon(Icons.leaderboard), findsOneWidget);
+    expect(find.text('Leaderboard'), findsOneWidget);
+    expect(find.byIcon(Icons.person), findsOneWidget);
+    expect(find.text('My Profile'), findsOneWidget);
+  });
+
+  group('BottomNavBar Tests', () {
+    testWidgets('Pressing leaderboard icon navigates to Leaderboard Page', (tester) async {
+      await tester.pumpWidget(const MaterialApp(home: MyHomePage(pageIndex: 0)));
+
+      expect(find.text('CHIMP GAME'), findsOneWidget);
+      expect(find.text('PLAY'), findsOneWidget);
+      expect(find.byIcon(Icons.home), findsOneWidget);
+      expect(find.byIcon(Icons.leaderboard), findsOneWidget);
+      expect(find.byIcon(Icons.person), findsOneWidget);
+
+      await tester.tap(find.byIcon(Icons.leaderboard));
+      await tester.pump();
+
+      expect(find.text('Leaderboard'), findsNWidgets(2));
+      expect(find.byIcon(Icons.leaderboard), findsNWidgets(2));
+
+      expect(find.text('CHIMP GAME'), findsNothing);
+      expect(find.text('PLAY'), findsNothing);
+      expect(find.byIcon(Icons.home), findsOneWidget);
+      expect(find.byIcon(Icons.person), findsOneWidget);
+    });
+
+    testWidgets('Pressing person icon navigates to Profile Page', (tester) async {
+      isGuest = true;
+      await tester.pumpWidget(const MaterialApp(home: MyHomePage(pageIndex: 0)));
+
+      expect(find.text('CHIMP GAME'), findsOneWidget);
+      expect(find.text('PLAY'), findsOneWidget);
+      expect(find.byIcon(Icons.home), findsOneWidget);
+      expect(find.byIcon(Icons.leaderboard), findsOneWidget);
+      expect(find.byIcon(Icons.person), findsOneWidget);
+
+      await tester.tap(find.byIcon(Icons.person));
+      await tester.pump();
+
+      expect(find.text('Guest'), findsOneWidget);
+      expect(find.text('Not registered yet?'), findsOneWidget);
+      expect(find.text('Click here to register!'), findsOneWidget);
+      expect(find.text('Already have an account?'), findsOneWidget);
+      expect(find.text('Click here to login!'), findsOneWidget);
+
+      expect(find.text('CHIMP GAME'), findsNothing);
+      expect(find.text('PLAY'), findsNothing);
+      expect(find.byIcon(Icons.home), findsOneWidget);
+      expect(find.byIcon(Icons.leaderboard), findsOneWidget);
+      expect(find.byIcon(Icons.person), findsOneWidget);
+    });
+
+    testWidgets('Pressing home icon navigates to Profile Page', (tester) async {
+      isGuest = true;
+      await tester.pumpWidget(const MaterialApp(home: MyHomePage(pageIndex: 2)));
+
+      expect(find.text('Guest'), findsOneWidget);
+      expect(find.text('Not registered yet?'), findsOneWidget);
+      expect(find.byIcon(Icons.home), findsOneWidget);
+      expect(find.byIcon(Icons.leaderboard), findsOneWidget);
+      expect(find.byIcon(Icons.person), findsOneWidget);
+
+      await tester.tap(find.byIcon(Icons.home));
+      await tester.pump();
+
+      expect(find.text('CHIMP GAME'), findsOneWidget);
+      expect(find.text('PLAY'), findsOneWidget);
+
+      expect(find.text('Guest'), findsNothing);
+      expect(find.text('Not registered yet?'), findsNothing);
+      expect(find.byIcon(Icons.home), findsOneWidget);
+      expect(find.byIcon(Icons.leaderboard), findsOneWidget);
+      expect(find.byIcon(Icons.person), findsOneWidget);
+    });
+  });
+}
